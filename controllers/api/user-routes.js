93adbc56d657136fbe27ccfc851799e850dd7697
Dimosthenis Pagkas <dimosthenispagkas@macbook-pro.myfiosgateway.com>
2022-06-04 10:52:43 -0400

    update signup js, login,logout buttons

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,18 +4,42 @@ const bcrypt = require("bcrypt")
 
 router.get("/", (req, res) => {})
 
-router.get("/:id", (req, res) => {})
+router.get("/:id", (req, res) => {
+    User.findOne({
+        attributes: { exclude: ["password"] },
+        where: {
+            id: req.params.id,
+        },
+        include: [
+            {
+                model: Movie,
+            },
+        ],
+    })
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({ message: "No user with this ID found." })
+                return
+            }
+
+            res.json(userData)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
 
 router.post("/", (req, res) => {})
 
 router.post("/login", (req, res) => {
     User.findOne({
         where: {
-            email: req.body.email,
+            username: req.body.userName,
         },
     }).then(dbUserData => {
         if (!dbUserData) {
-            res.status(400).json({ message: "No user with that email address!" })
+            res.status(400).json({ message: "No user with that username!" })
             return
         }
 
@@ -30,7 +54,7 @@ router.post("/login", (req, res) => {
     })
 })
 
-router.post("/logout", (req, res) => {
+router.get("/logout", (req, res) => {
     req.session.destroy(function (err) {
         // cannot access session here
         res.redirect("/")
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,3 +1,4 @@
+const userEmail = document.getElementById("email")
 const userPassword = document.getElementById("password")
 const userUsername = document.getElementById("username")
 
@@ -5,19 +6,21 @@ const submitButton = document.getElementById("submitbutton")
 
 submitButton.addEventListener("click", event => {
     event.preventDefault()
-    const loginData = {
+    const signupData = {
         userName: userUsername.value.trim(),
+        userEmail: userEmail.value.trim(),
         userPassword: userPassword.value.trim(),
     }
-    if (!loginData.userName || !loginData.userPassword) {
+    if (!signupData.userName || !signupData.userEmail || !signupData.userPassword) {
+        // brodcast user message"not valid data"
         return
     }
-    fetch("/api/login", {
+    fetch("/api/signup", {
         method: "POST",
         headers: {
             Accept: "application/json",
             "Content-type": "application/json",
         },
-        body: JSON.stringify(loginData),
-    }).then(fetch("/dashboard"))
+        body: JSON.stringify(signupData),
+    })
 })
