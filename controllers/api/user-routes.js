fcdb953f2ea2f2b4613ec796c1d73907e3c670e5
Dimosthenis Pagkas <dimosthenispagkas@macbook-pro.myfiosgateway.com>
2022-06-08 21:14:40 -0400

    changing the front end

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,13 +4,13 @@ const bcrypt = require("bcrypt")
 
 router.get("/", (req, res) => {
     User.findAll({
-        attributes: { exclude: ['password'] }
+        attributes: { exclude: ["password"] },
     })
-    .then(userData => res.json(userData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
 })
 
 router.get("/:id", (req, res) => {
@@ -49,9 +49,8 @@ router.post("/login", (req, res) => {
             res.status(400).json({ message: "No user with that username!" })
             return
         }
-        
 
-        const validPassword = bcrypt.compareSync(req.body.password, userData.password);
+        const validPassword = bcrypt.compareSync(req.body.password, userData.password)
 
         if (!validPassword) {
             res.status(400).json({ message: "Incorrect password!" })
@@ -59,64 +58,63 @@ router.post("/login", (req, res) => {
         }
 
         req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.username = userData.username;
-            req.session.loggedIn = true;
-        
-            res.json({ user: userData, message: 'You are now logged in!' });
-        });
+            req.session.user_id = userData.id
+            req.session.username = userData.username
+            req.session.loggedIn = true
+
+            res.json({ user: userData, message: "You are now logged in!" })
+        })
     })
 })
 
 router.post("/logout", (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
-          res.status(204).end();
-        });
-      }
-      else {
-        res.status(404).end();
-      }
+            res.status(204).end()
+        })
+    } else {
+        res.status(404).end()
+    }
 })
 
 router.post("/signup", (req, res) => {
     User.create({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
-      })
+        password: req.body.password,
+    })
         .then(userData => {
-          req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.username = userData.username;
-            req.session.loggedIn = true;
-      
-            res.json(userData);
-          });
+            req.session.save(() => {
+                req.session.user_id = userData.id
+                req.session.username = userData.username
+                req.session.loggedIn = true
+
+                res.json(userData)
+            })
         })
         .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+            console.log(err)
+            res.status(500).json(err)
+        })
 })
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
     User.destroy({
-      where: {
-        id: req.params.id
-      }
+        where: {
+            id: req.params.id,
+        },
     })
-      .then(userData => {
-        if (!userData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(userData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-  
-module.exports = router;
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({ message: "No user found with this id" })
+                return
+            }
+            res.json(userData)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+module.exports = router
